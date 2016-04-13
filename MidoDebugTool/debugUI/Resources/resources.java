d0c987c6def9f811eea0dc6609e4be90748656f7
Minh Nguyen <minh.filestring@midoplay.com>
2016-04-14 04:26:37 +0700

    adding complete draw with winning numbers

--- MidoDebugTool/debugUI/Resources/resources.java
+++ MidoDebugTool/debugUI/Resources/resources.java
@@ -1,21 +1,25 @@
 package Resources;
-import java.awt.image.ComponentColorModel;
+
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.*;
-import java.nio.channels.ShutdownChannelGroupException;
-
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.sun.org.apache.xml.internal.security.keys.ContentHandlerAlreadyRegisteredException;
+
+import org.apache.*;
 
 
 
 
 public class resources {
-	public final String STAGING_URL = "https://stg-api.midoplay.com/api";
+	public final String STAGING_URL = "stg-api.midoplay.com/api";
 	public final String DEMO_URL = "https://demo-api.midoplay.com/api";
 	public final String STAGING = "Staging";
 	public final String DEMO = "Demo";
@@ -26,10 +30,7 @@ public class resources {
 	
 	public void showErrorMessage(String title, String errorMessage){
 		JOptionPane.showMessageDialog(null, errorMessage, title, JOptionPane.ERROR_MESSAGE);
-		
 	}
-	
-	
 	
 	
 	public void closeDraw(String environment){
@@ -37,126 +38,65 @@ public class resources {
 		HttpURLConnection connection = null;
 	
 		
-		try {
-			if (environment == null){
-				showErrorMessage("Error", "Environment is null!");
-				return;
-			}
-			if (environment.equals(STAGING)){
-				midoURL = new URL(STAGING_URL);
-			}
-			else {
-				midoURL = new URL(DEMO_URL);
-			}
-			midoURL = new URL("https://stg-api.midoplay.com/api/service");
-			connection = (HttpURLConnection)midoURL.openConnection();
-		    connection.setRequestMethod("GET");
-		    connection.setRequestProperty("Content-Type", "application/json");
-		    connection.setRequestProperty("Authorization", "Bearer MTQ2MDUzNjYwNDQxOTpiMTM2Y2Y0YzAzZWI1NmRlOGU4MDJhOWFiZWVhMmRiNjU3NGU4MDRkMTA3YWE2MmEyODBlN2ExNmY0ZTNmMDY2OlVTRVJfTE9HSU46Om10NUB5b3BtYWlsLmNvbTo6X186Ol9fOjoxMTUuNzkuNTUuMTc3OjoyODU0YWRmNy05OWM5LTQyNTEtOWU2Yy0xNGJiOTRkYWZlNDQ6YzI0ODQ0YzU1NjdkMjU5ZWRmODBmMWJmYzQyNDk0YmZjYmFkN2Y3Zjc0NjYwNTJhMDRkYTQxNDI3ZjE4OWQ0ODZjMTk5NzVmYzAwMDQ3NDcyNDQ0MWM3ZTg1ZmFhNjRkMmE4MzI2ODhiMmViNjNmYjVjNzYxOTA1YzUyMWFhMjg=");
-		    
-		    //send request
-		    
-		    int responseCode = connection.getResponseCode();
-		    
-		    
-		    
-			
-		    //Get Response  
-	        InputStream is = connection.getInputStream();
-	        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-	        StringBuilder response = new StringBuilder(); // or StringBuffer if not Java 5+ 
-	        String line;
-	        while((line = rd.readLine()) != null) {
-	          response.append(line);
-	          response.append('\r');
-	        }
-	        rd.close();
-	        System.out.print(response.toString());
-			
-			return;
-			
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if(connection != null) {
-				connection.disconnect();
-			}
-		}
 		
-		
-		showErrorMessage("", "");
 	}
 	
 	
 	
-	public String[] login(String username, String password){
+	public String login(String username, String password){
 		String result = null;
 		String authToken = null;
+		String parameters = "emailAddress=" + username + "&" + "password=" + password;  
 		
-		
-		
-		
-		
-		
-		
-		
-		showErrorMessage("ERROR", "this is an error message");
-		return new String[]{result, authToken};
+		String response = sendPost("/login", null, parameters);
+		System.out.println(response);
+		return response;
 	}
 	
 	
 	
+	public String getService(String authToken){
+		String a = sendGet("/service", authToken);
+		return a;
+	}
 	
 	
-	
-	
-	
-	public void sendGet(String uri, String authToken){
+	/*
+		private methods
+	*/
+	private String sendGet(String uri, String authToken){
 		String url = null;
 		
-		if (environment.equals("Staging")){
-			url = STAGING_URL;
+		if (environment.equals(STAGING)){
+			url = STAGING_URL + uri;
 		} else {
-			url = DEMO_URL;
+			url = DEMO_URL + uri;
 		}
+		HttpGet get = new HttpGet();
+		URIBuilder a = new URIBuilder();
 
-		try {
-			URL obj = new URL(url);
-			HttpURLConnection con;
-			con = (HttpURLConnection) obj.openConnection();
 		
-
-			// optional default is GET
-			con.setRequestMethod("GET");
-			con.setRequestProperty("Content-Type", "application/json");
-			con.setRequestProperty("Authorization", authToken);
-		    
-			//add request header
-	
-			int responseCode = con.getResponseCode();
-			System.out.println("\nSending 'GET' request to URL : " + url);
-			System.out.println("Response Code : " + responseCode);
-	
-			BufferedReader in = new BufferedReader(
-			        new InputStreamReader(con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-	
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
-	
-			//print result
-			System.out.println(response.toString());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		return null;
 	}
 	
+	
+	private String sendPost(String uri, String authToken, String parameters){
+		String url = null;
+		if (environment.equals("Staging")){
+			url = STAGING_URL + uri;
+		} else {
+			url = DEMO_URL + uri;
+		}
+		HttpPost post = null;
+		
+		
+		
+		
+		
+		
+		
+		return null;
+	}
+
 	
 }
--- MidoDebugTool/debugUI/com/midodebug/tool/MidoDebugTool.java
+++ MidoDebugTool/debugUI/com/midodebug/tool/MidoDebugTool.java
@@ -13,10 +13,13 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import Resources.resources;
+import javax.swing.JTextField;
+import javax.swing.JRadioButton;
 
 public class MidoDebugTool extends JFrame{
 
 	private JPanel contentPane;
+	private JTextField txtWinningNumbers;
 
 	/**
 	 * Launch the application.
@@ -38,9 +41,10 @@ public class MidoDebugTool extends JFrame{
 	 * Create the frame.
 	 */
 	public MidoDebugTool() {
-		setTitle("Tool");
+		setResizable(false);
+		setTitle("Mido Debug Tool");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 555, 327);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
@@ -49,36 +53,114 @@ public class MidoDebugTool extends JFrame{
 		
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
 		
-		JLabel lblApplicationTitle = new JLabel("Tool");
-		panel.add(lblApplicationTitle);
+		JButton btnCloseDraw = new JButton("Close/Complete Mido Millions Draw");
+		btnCloseDraw.setBounds(38, 110, 247, 46);
 		
-		JButton btnCloseDraw = new JButton("New button");
+		panel.add(btnCloseDraw);
+		
+		txtWinningNumbers = new JTextField();
+		txtWinningNumbers.setHorizontalAlignment(SwingConstants.CENTER);
+		txtWinningNumbers.setText("01 02 03 04 05 06");
+		txtWinningNumbers.setBounds(315, 108, 145, 49);
+		panel.add(txtWinningNumbers);
+		txtWinningNumbers.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Mido Millions Draw Status");
+		lblNewLabel.setBounds(199, 198, 202, 16);
+		panel.add(lblNewLabel);
+		
+		JButton btnGetMidoStatus = new JButton("Get Mido Millions Draw Status");
+		
+		btnGetMidoStatus.setBounds(160, 228, 241, 39);
+		panel.add(btnGetMidoStatus);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Staging");
+		rdbtnNewRadioButton.setBounds(164, 49, 141, 23);
+		rdbtnNewRadioButton.setSelected(true);
+		
+		panel.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Demo");
+		rdbtnNewRadioButton_1.setBounds(286, 49, 141, 23);
+		panel.add(rdbtnNewRadioButton_1);
+		
+		JLabel lblSelectEnvironment = new JLabel("Select environment");
+		lblSelectEnvironment.setBounds(199, 21, 125, 16);
+		panel.add(lblSelectEnvironment);
+		
+		
+		
+		
+		
+		rdbtnNewRadioButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				rdbtnNewRadioButton.setSelected(true);
+				rdbtnNewRadioButton_1.setSelected(false);
+			}
+		});
+		
+		rdbtnNewRadioButton_1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				rdbtnNewRadioButton_1.setSelected(true);
+				rdbtnNewRadioButton.setSelected(false);
+			}
+		});
+		
+		
+		
+		
+		
+		
+		// get Mido Millions draw status
+		
+		btnGetMidoStatus.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				resources temp = new resources();
+				if (rdbtnNewRadioButton.isSelected()==true)
+					temp.environment = temp.STAGING;
+				else
+					temp.environment = temp.DEMO;
+				
+				String auth_token = temp.login("mt1@yopmail.com", "qwerty");
+				temp.getService("");
+				
+				
+				
+			}
+		});
+		
+		
+		// close draw
+		
 		btnCloseDraw.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				closeMidoDraw();
 			}
 		});
-		panel.add(btnCloseDraw);
+		
 	}
 	
 	public void closeMidoDraw(){
-		JOptionPane.showMessageDialog(null, "OK great, let's close this popup");
-		resources temp = new resources(); 
-		String[] a = temp.login("mt5@yopmail.com", "qwerty");
-				
-		if(a[0].equals("false"))
-			temp.showErrorMessage("ERROR", "Can't login at this time.\nYour account: " + "mt5@yopmail.com");
-		else{
-			
+//		JOptionPane.showMessageDialog(null, "OK great, let's close this popup");
 		
-		}
-		
-		temp.sendGet("Bearer MTQ2MDUzNjYwNDQxOTpiMTM2Y2Y0YzAzZWI1NmRlOGU4MDJhOWFiZWVhMmRiNjU3NGU4MDRkMTA3YWE2MmEyODBlN2ExNmY0ZTNmMDY2OlVTRVJfTE9HSU46Om10NUB5b3BtYWlsLmNvbTo6X186Ol9fOjoxMTUuNzkuNTUuMTc3OjoyODU0YWRmNy05OWM5LTQyNTEtOWU2Yy0xNGJiOTRkYWZlNDQ6YzI0ODQ0YzU1NjdkMjU5ZWRmODBmMWJmYzQyNDk0YmZjYmFkN2Y3Zjc0NjYwNTJhMDRkYTQxNDI3ZjE4OWQ0ODZjMTk5NzVmYzAwMDQ3NDcyNDQ0MWM3ZTg1ZmFhNjRkMmE4MzI2ODhiMmViNjNmYjVjNzYxOTA1YzUyMWFhMjg=");
+//		String[] a = temp.login("mt5@yopmail.com", "qwerty");
+//				
+//		if(a[0].equals("false"))
+//			temp.showErrorMessage("ERROR", "Can't login at this time.\nYour account: " + "mt5@yopmail.com");
+//		else{
+//			
+//		
+//		}
+		resources temp = new resources();
+		temp.environment = "Staging";
+		String b = temp.getService("Bearer MTQ2MDUzNjYwNDQxOTpiMTM2Y2Y0YzAzZWI1NmRlOGU4MDJhOWFiZWVhMmRiNjU3NGU4MDRkMTA3YWE2MmEyODBlN2ExNmY0ZTNmMDY2OlVTRVJfTE9HSU46Om10NUB5b3BtYWlsLmNvbTo6X186Ol9fOjoxMTUuNzkuNTUuMTc3OjoyODU0YWRmNy05OWM5LTQyNTEtOWU2Yy0xNGJiOTRkYWZlNDQ6YzI0ODQ0YzU1NjdkMjU5ZWRmODBmMWJmYzQyNDk0YmZjYmFkN2Y3Zjc0NjYwNTJhMDRkYTQxNDI3ZjE4OWQ0ODZjMTk5NzVmYzAwMDQ3NDcyNDQ0MWM3ZTg1ZmFhNjRkMmE4MzI2ODhiMmViNjNmYjVjNzYxOTA1YzUyMWFhMjg=");
+//		String b = temp.login("mt7@yopmail.com", "qwerty");
+		System.out.println(b);
 	}
-	
-	
-	
-	
-
 }
